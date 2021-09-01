3bd585cceaac40c0edae203d388e63a2c1144b82
Saravanakumars001 <sarans85@gmail.com>
2021-09-01 19:32:55 +0530

    Added class file

--- FSDConsoleApp/Program.cs
+++ FSDConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace FSDConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Lets Learn C#");
         }
     }
 }
