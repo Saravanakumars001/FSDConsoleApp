096f2453608a9368f6a78d969a1e0610654e56cb
Saravanakumars001 <sarans85@gmail.com>
2021-11-17 12:57:59 +0530

    Commit Web App development

---
+++ ConfigureMVCWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigureMVCWebApplication.Controllers
+{   
+    public class HomeController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            return View();
+        }
+        public ViewResult Contact()
+        {
+            return View("ContactPage");
+        }
+
+
+        public ActionResult Privacy()
+        {
+            return RedirectToAction("Index");
+        }
+        public string Message()
+        {
+            return "This is Message action method from Home controller";
+        }
+    }
+}
--- WebDevelopment/Startup.cs
+++ WebDevelopment/Startup.cs
@@ -22,7 +22,7 @@ namespace WebDevelopment
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+           // services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,16 +37,24 @@ namespace WebDevelopment
                 app.UseExceptionHandler("/Error");
             }
 
+
+
+            DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            defaultFilesOptions.DefaultFileNames.Clear();
+            defaultFilesOptions.DefaultFileNames.Add("dashboard.html");
+            defaultFilesOptions.DefaultFileNames.Add("home.html");
+
+            app.UseDefaultFiles(defaultFilesOptions);
             app.UseStaticFiles();
 
-            app.UseRouting();
+           /* app.UseRouting();
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
-            });
+            });*/
         }
     }
 }
